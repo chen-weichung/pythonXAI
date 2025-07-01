106ecd5747ed7a692991333ac81a0b6052e740db
chen-weichung <weichung.chen980715@gmail.com>
2025-07-01 07:04:43 +0000

    更新 class2-2.py，新增按鈕範例及相關功能

--- pages/class2-2.py
+++ pages/class2-2.py
@@ -19,3 +19,17 @@ elif a >= 60:
     st.write("D")
 else:
     st.write("F")
+
+import streamlit as st
+
+st.markdown("---")
+st.markdown("## 按鈕範例")
+# st.button("按鈕") 可以在網頁上顯示一個按鈕，使用者可以點擊按鈕
+# key是按鈕的識別名稱，可以用來區分不同的按鈕
+# 如果使用者點擊按鈕，st.button() 會回傳 True，否則回傳 False
+st.button("按我一下", key="button1")
+if st.button("按我一下", key="balloons"):
+    st.balloons()
+if st.button("按我一下", key="snow"):
+    st.snow()
+st.markdown("---")
