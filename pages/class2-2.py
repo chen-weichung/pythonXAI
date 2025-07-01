a630d6ac5c12ba2a16cde5586751f9eb41f2be72
chen-weichung <weichung.chen980715@gmail.com>
2025-07-01 06:45:04 +0000

    更新 class2-2.py，新增成績輸入功能及對應評分，移除不必要的數字輸入範例；新增 class2-3.py 檔案

--- pages/class2-2.py
+++ pages/class2-2.py
@@ -1,5 +1,21 @@
+# import streamlit as st
+
+# number = st.number_input("請輸入一個數字", step=1.5, min_value=-12.5, max_value=100.0)
+# # step=1 表示單位以及每次增加或減少的數量，min_value 表示最小值，max_value 表示最大值
+# st.write(f"你輸入的數字是: {number}")
+
 import streamlit as st
 
-number = st.number_input("請輸入一個數字", step=1.5, min_value=-12.5, max_value=100.0)
-# step=1 表示單位以及每次增加或減少的數量，min_value 表示最小值，max_value 表示最大值
-st.write(f"你輸入的數字是: {number}")
+st.title("練習")
+st.write("輸入成績")
+a = st.number_input("請輸入成績", step=1, min_value=0, max_value=100, value=50)
+if a >= 90:
+    st.write("A")
+elif a >= 80:
+    st.write("B")
+elif a >= 70:
+    st.write("C")
+elif a >= 60:
+    st.write("D")
+else:
+    st.write("F")
