dfdefdd8a7faa3d1971307332ccb075c127e8b71
chen-weichung <weichung.chen980715@gmail.com>
2025-07-02 13:30:05 +0800

    更新 class2-4.py，調整數字輸入範圍並新增星形圖案顯示功能；新增 class2-5.py，實作列表示範程式

--- pages/class2-4.py
+++ pages/class2-4.py
@@ -1,7 +1,22 @@
+# import streamlit as st
+
+# st.title("練習：for 迴圈")
+# st.write("請輸入一個數字(1-9)")
+# a = st.number_input("請輸入一個整數", step=1, min_value=0, max_value=9)
+# for i in range(a + 1):
+#     st.write(f"{i*str(i)}")
+
 import streamlit as st
 
 st.title("練習：for 迴圈")
-st.write("請輸入一個數字(1-9)")
-a = st.number_input("請輸入一個整數", step=1, min_value=0, max_value=9)
-for i in range(a + 1):
-    st.write(f"{i*str(i)}")
+st.write("請輸入一個數字")
+a = st.number_input("請輸入一個整數", step=1, min_value=0, max_value=5)
+total = ""
+for i in range(1, a + 1):
+    c = " " * (a - i)
+    d = "*" * (2 * i - 1)
+    total += f"{c}{d}\n"
+for i in range(1, a + 1):
+    e = " " * (a - 1) + "*"
+    total += f"{e}\n"
+st.markdown(f"```\n{total}```")
